66b728d2176466448af5b9b77260abc9cf15a56b
Mior Muhammad Zaki <crynobone@gmail.com>
2020-10-26 14:40:29 +0800

    Add reproducing code for laravel/nova-issues#2757
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -3,6 +3,7 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\DateTime;
 use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Password;
@@ -60,6 +61,8 @@ class User extends Resource
                 ->onlyOnForms()
                 ->creationRules('required', 'string', 'min:8')
                 ->updateRules('nullable', 'string', 'min:8'),
+
+            DateTime::make('Created At')->sortable(),
         ];
     }
 
